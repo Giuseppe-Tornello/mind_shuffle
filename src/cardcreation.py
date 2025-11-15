21da918b299def31bdf52cf20d96b459945a0c02
bored_dam <damianotrovato122@gmail.com>
2025-11-16 00:35:00 +0100

    feat(card_from_string) added new function in cardcreation.py related to card creation

--- src/cardcreation.py
+++ src/cardcreation.py
@@ -1,7 +1,10 @@
 import json
 import os
 
+
 def card_from_strings(question:str, answer:str, tip:str|None, tags:list[str]) -> dict:
+    """creates a flash card from the args"""
+
     card = {
         "question": question,
         "answer": answer,
@@ -12,25 +15,4 @@ def card_from_strings(question:str, answer:str, tip:str|None, tags:list[str]) ->
     return card
 
 
-def write_card_to_deck(card, deck_name:str) -> None:
-    path = "./data/decks/" + deck_name + ".json"
-
-    if not os.path.exists(path):
-        deck = []
-
-    else:
-        with open(path, "r", encoding="utf-8") as f:
-            try:
-                deck = json.load(f)
-                head = deck[len(deck)-1]
-                next_id = head.get('id') + 1
-            
-            except json.JSONDecodeError:
-                deck = []
-                next_id = 0
-    card.update({'id': next_id})
-    deck.append(card)
-    
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(deck, f, ensure_ascii=False, indent=4)
 

066f27a0b52c26a06656e076cad292c8f57a02b7
bored_dam <damianotrovato122@gmail.com>
2025-11-16 00:49:15 +0100

    feat(cardcreation.py): added function to write a card to a specified deck or a new one

--- src/cardcreation.py
+++ src/cardcreation.py
@@ -15,4 +15,28 @@ def card_from_strings(question:str, answer:str, tip:str|None, tags:list[str]) ->
     return card
 
 
+def write_card_to_deck(card:dict, deck_name:str) -> None:
+    """writes the choice card to the specified deck"""
 
+    path = "./data/decks/" + deck_name + ".json"
+
+    if not os.path.exists(path):
+        deck = []
+
+    else:
+        with open(path, "r", encoding="utf-8") as f:
+            try:
+                deck = json.load(f)
+                head = deck[len(deck)-1]
+                next_id = head.get('id') + 1
+            
+            except json.JSONDecodeError: 
+                # if the deck is not correctly formatted it's overwritten
+                deck = []
+                next_id = 0
+    
+    card.update({'id': next_id})
+    deck.append(card)
+    
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(deck, f, ensure_ascii=False, indent=4)

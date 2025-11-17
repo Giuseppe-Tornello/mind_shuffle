9280137c071765929469197caca1372cc8a20939
bored_dam <damianotrovato122@gmail.com>
2025-11-17 19:19:35 +0100

    refactor(cardcreation.py): created function to make the conversion from deck name to path name easier.

--- src/cardcreation.py
+++ src/cardcreation.py
@@ -2,6 +2,10 @@ import json
 import os
 from .data.constants import JSON_ENCODING, USER_PATH, DECKS_PATH, DECKS_EXTENSION
 
+def _deck_name_to_path(name:str) -> str:
+    """formats the deck name into a full relative path"""
+    return DECKS_PATH + name + DECKS_EXTENSION
+
 
 def card_from_strings(question:str, answer:str, tip:str|None, tags:list[str]) -> dict:
     """creates a flash card from the args"""
@@ -19,7 +23,7 @@ def card_from_strings(question:str, answer:str, tip:str|None, tags:list[str]) ->
 def write_card_to_deck(card:dict, deck_name:str) -> None:
     """writes the choice card to the specified deck"""
 
-    path = DECKS_PATH + deck_name + DECKS_EXTENSION
+    path = _deck_name_to_path(deck_name)
 
     if not os.path.exists(path):
         deck = []
@@ -51,13 +55,13 @@ def import_deck(deck:list[dict], deck_name:str) -> None:
     with the same name.
     """
     
-    path = DECKS_PATH + deck_name + DECKS_EXTENSION
+    path = _deck_name_to_path(deck_name)
     i = 0
 
     while os.path.exists(path):
         # needed to avoid overwriting other already locally existing decks
         i += 1
-        path = DECKS_PATH + deck_name + str(i) + DECKS_EXTENSION
+        path = _deck_name_to_path(deck_name + str(i))
 
     with open(path, "w", encoding=JSON_ENCODING) as f:
         json.dump(deck, f, ensure_ascii=False, indent=2)

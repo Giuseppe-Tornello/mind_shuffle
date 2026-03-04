afdcfae301e7701f7ea21ad5a5027a6e4d40b01b
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2026-03-04 19:28:10 +0100

    tests: added test for is_valid_deck_file

--- tests/test_deckcheck.py
+++ tests/test_deckcheck.py
@@ -1,11 +1,33 @@
-from src.deckcheck import is_valid_deck_extension
+from src.deckcheck import is_valid_deck_extension, is_valid_deck_file
 
 
 def test_is_valid_deck_extension() -> None:
     right_str = "/path/to/file/ending/with.json"
     wrong_str = "/path/to/file/ending/with.png"
-    assert type(is_valid_deck_extension(right_str)) is bool
+    assert isinstance(is_valid_deck_extension(right_str), bool)
     assert is_valid_deck_extension(right_str) is True
 
-    assert type(is_valid_deck_extension(wrong_str)) is bool
+    assert isinstance(is_valid_deck_extension(wrong_str), bool)
     assert is_valid_deck_extension(wrong_str) is False
+
+
+def test_is_valid_deck_file() -> None:
+    TEST_DECKS_PATH = "tests/test_decks/"
+    valid_deck_names = ["valid_deck1.json", "valid_deck2.json"]
+    invalid_deck_names = [
+        "invalid_deck1.json",
+        "invalid_deck2.html",
+        "invalid_deck3.json",
+        "invalid_deck4.json",
+        "invalid_deck5.json",
+        ]
+
+    for valid in valid_deck_names:
+        temp_deck_path = TEST_DECKS_PATH + valid
+        assert isinstance(is_valid_deck_file(temp_deck_path), bool)
+        assert is_valid_deck_file(temp_deck_path) is True
+
+    for invalid in invalid_deck_names:
+        temp_deck_path = TEST_DECKS_PATH + invalid
+        assert isinstance(is_valid_deck_file(temp_deck_path), bool)
+        assert is_valid_deck_file(temp_deck_path) is False

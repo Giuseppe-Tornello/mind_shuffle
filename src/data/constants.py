6571649dc969b2a22203df27aee5413d9a588e59
Giuseppe Tornello <giuseppe.tornello05@protonmail.com>
2026-03-12 15:07:31 +0100

    style: minor improvings

--- src/data/constants.py
+++ src/data/constants.py
@@ -1,4 +1,5 @@
 """Constants used throughout the code"""
+from typing import TypedDict
 
 USER_PATH = "storage/user/"
 DECKS_PATH = "storage/decks/"
@@ -6,3 +7,11 @@ DECKS_EXTENSION = ".json"
 CARD_ATTRIBUTES = {"question", "answer", "tip", "tags", "id"}
 OPTIONAL_ATTRIBUTES = {"tip"}
 JSON_ENCODING = "utf-8"
+
+
+class Flashcard(TypedDict):
+    question: str
+    answer: str
+    tip: str
+    tags: list[str]
+    id: int
